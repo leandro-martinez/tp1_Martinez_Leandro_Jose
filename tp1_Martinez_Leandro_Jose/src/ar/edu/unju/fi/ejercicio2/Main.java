a297588791ba1dc748ea59069ca8e5252d08f12d
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-08 19:59:41 -0300

    resolucion del ejercicio 2

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -3,7 +3,23 @@ package ar.edu.unju.fi.ejercicio2;
 public class Main {
 
 	public static void main(String[] args) {
+		String nombrePais, numeroTelefono;
+		int edadPersona;
+		double alturaEdificio, precioProducto, coseno;
+		
+		nombrePais = "Argentina";
+        edadPersona = 20;
+        alturaEdificio = 145.5; 
+        precioProducto = 34.99;
+        numeroTelefono = "+54119999";
+        coseno = Math.cos(0.5);
 
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Edad de la persona: " + edadPersona + " años");
+        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+        System.out.println("Precio del producto: $" + precioProducto);
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Coseno de 0.5: " + coseno);
 	}
 
 }
