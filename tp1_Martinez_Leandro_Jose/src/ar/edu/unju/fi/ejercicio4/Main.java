e271cdcdbb4800037b8a06367839a8541222ef06
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-08 21:21:32 -0300

    ejercicio4 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -1,9 +1,29 @@
 package ar.edu.unju.fi.ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero dentro del rango [0, 10]");
+		int numero = scanner.nextInt();
+		
+        if (numero < 0 || numero > 10) {
+            System.out.println("El número debe estar dentro del rango [0, 10]");
+        } else {
+        	scanner.close();
+            int factorial = 1;
+            int contador = 1;
 
+            while (contador <= numero) {
+                factorial *= contador;
+                contador++;
+            }
+
+            System.out.println("El factorial de " + numero + " es: " + factorial);
+        }
 	}
 
 }
