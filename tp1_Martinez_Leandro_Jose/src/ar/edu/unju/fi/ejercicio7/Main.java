538438a4be8431057d203147d0dbae0e8c7659e7
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-09 20:39:32 -0300

    ejercicio 7 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio7/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio7/Main.java
@@ -1,10 +1,29 @@
 package ar.edu.unju.fi.ejercicio7;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio7.model.Empleado;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ingrese los datos: ");
+        System.out.println("Nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Legajo: ");
+        Integer legajo = scanner.nextInt();
+        System.out.println("Salario: ");
+        Double salario = scanner.nextDouble();
+
+        // Crear el objeto Empleado
+        Empleado empleado = new Empleado(nombre, legajo, salario);
+        empleado.mostrarDatos();
+        empleado.darAumento();
+        System.out.println("----------");
+        empleado.mostrarDatos();
+        
 	}
 
 }
--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
@@ -1,5 +1,56 @@
 package ar.edu.unju.fi.ejercicio7.model;
 
 public class Empleado {
+	private String nombre;
+	private Integer legajo;
+	private Double salario;
+	private static final Double SALARIO_MINIMO = 210000.00;
+	private static final Double AUMENTO_POR_MERITOS = 20000.00;
+	
+	public Empleado () {}
 
+	public Empleado(String nombre, Integer legajo, Double salario) {
+		super();
+		this.nombre = nombre;
+		this.legajo = legajo;
+		if(salario >= SALARIO_MINIMO) {
+			this.salario = salario;			
+		} else {
+			this.salario = SALARIO_MINIMO;
+		}
+	}
+	
+	public void mostrarDatos () {
+		System.out.println("Nombre del empleado: " + this.getNombre() + "\n" + "Legajo: " + this.getLegajo()
+		+ "\n" + "Salario: $" + this.getSalario());
+	}
+	
+	public void darAumento () {
+		this.salario = salario + AUMENTO_POR_MERITOS;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(Integer legajo) {
+		this.legajo = legajo;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
 }
