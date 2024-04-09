3c48a3ccf987c9097c2d13de279fe487b81b10a6
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-08 21:05:02 -0300

    ejercicio3 realizado

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio3/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -1,9 +1,20 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero entero: ");
+		int numero = sc.nextInt();
+		if(numero % 2 == 0) {
+			System.out.println("Triple: " + numero * 3);
+		}else {
+			System.out.println("Doble: " + numero * 2);
+		}
+		sc.close();
 	}
 
 }
