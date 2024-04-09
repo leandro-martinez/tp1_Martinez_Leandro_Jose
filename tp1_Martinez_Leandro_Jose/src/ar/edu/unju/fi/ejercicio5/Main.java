340474a76a65cd403b366cbaba78a23f81b2e8a6
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-08 21:30:54 -0300

    ejercicio5 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -1,9 +1,22 @@
 package ar.edu.unju.fi.ejercicio5;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero dentro del rango [1,9]");
+		int numero = scanner.nextInt();
+		
+		if(numero < 1 || numero > 9) {
+			System.out.println("El numero debe estar dentro del rango [1,9]");
+		} else {
+			for(int i = 1; i <= 10; i++) {
+				System.out.println(numero + " * " + i + " = " + numero * i);
+			}
+		}
 	}
 
 }
