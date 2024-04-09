cd5da5be4c39eedda0e51a8d3705c4f8ab231c46
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-09 20:50:04 -0300

    creacion de paquetes y clases para ejercicio8

---
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio8/Main.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.ejercicio8;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio8/model/CalculadoraEspecial.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.ejercicio8.model;
+
+public class CalculadoraEspecial {
+
+}
