80bbd264560af2fd0684ea3fac4c7deb19b7cf7b
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-09 19:54:53 -0300

    ejercicio 6 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/Main.java
@@ -1,9 +1,35 @@
 package ar.edu.unju.fi.ejercicio6;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio6.model.Persona;
+
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        // Crear hasta 3 objetos Persona
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Ingrese los datos para la persona " + i + ":");
+            System.out.print("DNI: ");
+            String dni = scanner.nextLine();
 
+            System.out.print("Nombre: ");
+            String nombre = scanner.nextLine();
+
+            System.out.print("Fecha de nacimiento (formato AAAA-MM-DD): ");
+            String fechaNacimientoStr = scanner.nextLine();
+            LocalDate fechaNacimiento = LocalDate.parse(fechaNacimientoStr);
+
+            System.out.print("Provincia: ");
+            String provincia = scanner.nextLine();
+
+            // Crear el objeto Persona
+            Persona persona = new Persona(dni, nombre, fechaNacimiento, provincia);
+            persona.mostrarDatos();
+            System.out.println("----------"); 
+        }
 	}
 
 }
--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -1,5 +1,89 @@
 package ar.edu.unju.fi.ejercicio6.model;
 
-public class Persona {
+import java.time.LocalDate;
+import java.time.Period;
 
+public class Persona {
+	private String dni;
+	private String nombre;
+	private LocalDate fechaDeNacimiento;
+	private String provincia;
+	
+	public Persona () {}
+	
+	public int calcularEdad () {
+		 LocalDate fechaActual = LocalDate.now();
+		 int edad = fechaActual.getYear() - this.getFechaDeNacimiento().getYear();
+		 if (fechaActual.getMonthValue() < fechaDeNacimiento.getMonthValue() ||
+		    (fechaActual.getMonthValue() == fechaDeNacimiento.getMonthValue() &&
+		     fechaActual.getDayOfMonth() < fechaDeNacimiento.getDayOfMonth())) {
+		        edad--;
+		  }
+		 return edad;   
+	}
+	
+	public void mostrarDatos () {
+		String esMayorDeEdad;
+		if(verificarEdad()) {
+			esMayorDeEdad = "La persona es mayor de edad";
+		}else {
+			esMayorDeEdad = "La persona no es mayor de edad";
+		}
+		System.out.println("DNI: " + this.getDni() + " | " + "Nombre: " + this.getNombre() + " | " + "Fecha de Nacimiento: " + this.getFechaDeNacimiento() 
+		+ " | " + "Provincia: " + this.getProvincia() + " | " + "Edad: " + calcularEdad() + " | " + esMayorDeEdad);  
+	}
+	
+	public Boolean verificarEdad () {
+		return calcularEdad() >= 18;
+	}
+	
+	public Persona(String dni, String nombre, LocalDate fechaDeNacimiento, String provincia) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fechaDeNacimiento = fechaDeNacimiento;
+		this.provincia = provincia;
+	}
+	
+	public Persona(String dni, String nombre, LocalDate fechaDeNacimiento) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fechaDeNacimiento = fechaDeNacimiento;
+		this.provincia = "Jujuy";
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+
+	public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	
+	
 }
