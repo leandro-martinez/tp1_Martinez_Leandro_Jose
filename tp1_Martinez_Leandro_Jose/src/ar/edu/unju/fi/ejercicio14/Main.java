a0b0d8f1de6ee2be2bcd6beda2a8ca915d8f439b
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-10 16:21:11 -0300

    ejercicio 14 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio14/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio14/Main.java
@@ -1,9 +1,31 @@
 package ar.edu.unju.fi.ejercicio14;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) {		
+		int numero;
+		Scanner scanner = new Scanner(System.in);
+		do {
+			System.out.println("Ingrese un numero dentro del rango [3,10]");
+			numero = scanner.nextInt();
+		}while(!(numero >= 3 && numero <= 10));
+		int[] myArray = new int[numero];
+		
+		for(int i = 0; i < myArray.length; i++) {
+			System.out.println("Ingresa un numero: ");
+			myArray[i] = scanner.nextInt();
+		}
+		int sumaTotal = 0;
+		for(int i = 0; i < myArray.length; i++) {
+			System.out.println("Indice: " + i + " | " + "Valor: " + myArray[i]);
+			sumaTotal += myArray[i];
+		}
+		scanner.close();
+		System.out.println("Suma total: " + sumaTotal);
+		System.out.println("----- Fin del programa -----");
+		
 	}
 
 }
