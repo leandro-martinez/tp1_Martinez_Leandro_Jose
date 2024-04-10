38f5f5282f338b22fcb36b3163cb2ca320ead194
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-10 10:47:01 -0300

    ejercicio 11 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio11/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -3,7 +3,22 @@ package ar.edu.unju.fi.ejercicio11;
 public class Main {
 
 	public static void main(String[] args) {
-
+		int i = 2, j;
+		boolean band = true;
+		int valor = 0;
+		int desc = 1;
+		
+		for(j = 40; j >= 0; j--) {
+			if(band) {
+				valor = j * i;
+				band = false;
+				desc += i; 
+			} else {
+				System.out.print(valor + " ");
+				valor += (40 - desc);
+				desc += i;
+			}
+		}
 	}
 
 }
