cc2db2bfd205991d0b4d2c24390e2a5db2f167cb
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-10 16:08:24 -0300

    ejercicio 13 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio13/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -1,9 +1,21 @@
 package ar.edu.unju.fi.ejercicio13;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		int[] myArray = new int[8];
+		
+		for(int i = 0; i < myArray.length; i++) {
+			System.out.println("Ingrese un numero: ");
+			myArray[i] = scanner.nextInt();
+		}
+		for(int i = 0; i < myArray.length; i++) {
+			System.out.println("Indice: " + i + " | " + "Valor: " + myArray[i]);					
+		}
+		scanner.close();
 	}
 
 }
