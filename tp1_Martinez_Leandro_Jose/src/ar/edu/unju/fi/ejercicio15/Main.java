bd42f9ab5384d66386a9f42821bd290ee2e72592
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-10 16:46:53 -0300

    ejercicio 15 resuelto

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio15/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -1,9 +1,32 @@
 package ar.edu.unju.fi.ejercicio15;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
-	}
+		Scanner scanner = new Scanner(System.in);
+		int numero;
+		do {
+			System.out.println("Ingrese un numero dentro del rango [5,10]: ");
+			numero = scanner.nextInt();
+		} while(!(numero >= 5 && numero <=10));
+		
+		String[] myArray = new String[numero];
+		scanner.nextLine();
+		for(int i = 0; i < myArray.length; i++) {
+			System.out.println("Ingrese un nombre: ");
+			myArray[i] = scanner.nextLine();
+		}
+		System.out.println("----------");
+		for(int i = 0; i < myArray.length; i++) {
+			System.out.println("Indice: " + i + " | " + "Valor: " + myArray[i]);
+		}
+		System.out.println("----------");
+		for(int i = myArray.length - 1; i >= 0; i--) {
+			System.out.println("Indice: " + i + " | " + "Valor: " + myArray[i]);
+		}
+		scanner.close();
+	}	
 
 }
