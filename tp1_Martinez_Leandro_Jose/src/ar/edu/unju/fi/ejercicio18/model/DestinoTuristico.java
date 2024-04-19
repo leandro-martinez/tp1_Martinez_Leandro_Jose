9d7d9b21924f2ec680fa86d851044b0ed764405a
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-19 14:57:41 -0300

    creacion de clases DestinoTuristico y Pais

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio18/model/DestinoTuristico.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio18/model/DestinoTuristico.java
@@ -7,4 +7,55 @@ public class DestinoTuristico {
 	private Pais pais;
 	private Integer cantDias;
 	
+	public DestinoTuristico() {}
+	
+	public DestinoTuristico(String codigo, String nombre, Integer precio, Pais pais, Integer cantDias) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.pais = pais;
+		this.cantDias = cantDias;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	public Integer getCantDias() {
+		return cantDias;
+	}
+
+	public void setCantDias(Integer cantDias) {
+		this.cantDias = cantDias;
+	}
+	
 }
