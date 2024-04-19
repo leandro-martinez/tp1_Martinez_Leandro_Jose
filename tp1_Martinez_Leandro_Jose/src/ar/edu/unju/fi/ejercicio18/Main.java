94c7adacb63b1f0fd606a5b0787ebd332ebd0d16
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-19 15:44:59 -0300

    funciones para precargarPaises y mostrarPaises añadidas

--- tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio18/Main.java
+++ tp1_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio18/Main.java
@@ -1,13 +1,19 @@
 package ar.edu.unju.fi.ejercicio18;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio18.model.Pais;
 
 public class Main {
 	private static Scanner scanner;
+	private static List<Pais> paises;
 	
 	public static void main(String[] args) {
 		scanner = new Scanner(System.in);
+		precargarPaises();
 		
 		int option = 0;
 		try {
@@ -40,7 +46,7 @@ public class Main {
 					break;
 				case 6: System.out.println("opcion 6");
 					break;
-				case 7: System.out.println("opcion 7");
+				case 7: mostrarPaises();
 					break;
 				case 8: System.out.println("opcion 8");
 					break;
@@ -54,6 +60,18 @@ public class Main {
 		} catch(InputMismatchException ex){
 			System.out.println("Opcion no válida");
 		}
+	}
+	public static void precargarPaises () {
+		if(paises == null) {
+			paises = new ArrayList<>();
+		}
+		paises.add(new Pais("ARG", "Argentina"));
+		paises.add(new Pais("BOL", "Bolivia"));
+		paises.add(new Pais("CHL", "Chile"));	
+	}
+	public static void mostrarPaises() {
+		System.out.println("--------- Lista de Paises ----------");
+		paises.forEach(pais -> System.out.println(pais));
 	}
 
 }
